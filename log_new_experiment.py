0eec7f681753c35bc6a52c889e4004723fee660c
Cameron Carver <35936027+clcarver1130@users.noreply.github.com>
2018-10-17 12:49:18 -0500

    log_page_1 complete

--- log_new_experiment.py
+++ log_new_experiment.py
@@ -1,45 +1,56 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 
-def log_new_experiment(username, password):
+def connect_login(username, password, path_to_driver):
 
-        # Connect:
+        # Login:
         profile = webdriver.FirefoxProfile()
         profile.accept_untrusted_certs = True
-        browser = webdriver.Firefox(firefox_profile=profile, executable_path='/home/clcarver/geckodriver')
+        browser = webdriver.Firefox(firefox_profile=profile, executable_path=path_to_driver)
         browser.get('https://cmsdb.darkcosmos.org/experiments/run/new')
-
-        # Login
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div[5]/main/div/div[1]/div/div[3]/button'))).click()
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#username"))).send_keys(username)
         browser.find_element_by_css_selector("input#password").send_keys(password)
         login_attempt = browser.find_element_by_css_selector("div.v-btn__content>i.v-icon.pr-1.mdi.mdi-lock-open-outline.theme--light").click()
+        return browser
 
+def log_page_1(browser, led_list):
 
-        # Log new experiment:
-
-        # Page 1
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#title"))).send_keys('Reference IVP Curve - 30 steps') # Title
-        browser.find_element_by_css_selector("input#desc").send_keys('30 step IVP curve for performance tests') # Details
-        time.sleep(2)
+        # Title and details:
+        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#title"))).send_keys('Reference IVP Curve - 30 steps')
+        browser.find_element_by_css_selector("input#desc").send_keys('30 step IVP curve for performance tests')
 
         # Number of Groups:
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[12]/main/div/div[1]/div[2]/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/form/div[4]/div/div/div[2]/div[1]/div[1]/input'))).click() # Open first drop down
-        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[3]/div/div/div[1]/a/div/div'))).click() # Click group 1
+        WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[3]/div/div/div[1]/a/div/div'))).click()
 
         # Experiment Type:
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[21]/main/div/div[1]/div[2]/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/form/div[5]/div/div/div[2]/div[1]/div[1]/input'))).click()
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[9]/div/div/div[2]/a/div/div'))).click()
 
-        # LED:
+        # LEDs
+        for led in led_list:
+            browser.find_element_by_css_selector("div.v-select__selections:nth-child(2) > input:nth-child(1)").send_keys(led)
+            time.sleep(1)
+            browser.find_element_by_css_selector("div.v-select__selections:nth-child(2) > input:nth-child(1)").send_keys(Keys.RETURN)
+            browser.find_element_by_xpath('/html').click() # Click off screen to reset dropdown
 
+        # Submit
+        browser.find_element_by_css_selector('div.v-stepper__content:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(1)').click()
+        print('Page 1: Experiment Complete')
+        return browser
 
-
-        # Page 2
+def log_page_2(browser):
+    
 
 if __name__ == '__main__':
-    log_new_experiment(username, password)
+    browser = connect_login('bletson', 'baseball2046', '/Users/cameron.carverdata/geckodriver.exe')
+    time.sleep(2)
+    log_page_1(browser, led_list=['AA123', 'CJ03'])
+    time.sleep(2)
+    log_page_2(browser)
--- test.py
+++ test.py
@@ -1,30 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 
-def connect_login(username, password):
+def connect_login(username, password, path_to_driver):
 
         # Login:
         profile = webdriver.FirefoxProfile()
         profile.accept_untrusted_certs = True
-        browser = webdriver.Firefox(firefox_profile=profile, executable_path='/home/clcarver/geckodriver')
+        browser = webdriver.Firefox(firefox_profile=profile, executable_path=path_to_driver)
         browser.get('https://cmsdb.darkcosmos.org/experiments/run/new')
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div[5]/main/div/div[1]/div/div[3]/button'))).click()
-        # WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.v-content__wrap button.v-btn.v-btn--flat.theme--light.primary--text"))).click()
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#username"))).send_keys(username)
         browser.find_element_by_css_selector("input#password").send_keys(password)
         login_attempt = browser.find_element_by_css_selector("div.v-btn__content>i.v-icon.pr-1.mdi.mdi-lock-open-outline.theme--light").click()
         return browser
 
-def log_page_1(browser):
+def log_page_1(browser, led_list):
 
         # Title and details:
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#title"))).send_keys('Reference IVP Curve - 30 steps')
         browser.find_element_by_css_selector("input#desc").send_keys('30 step IVP curve for performance tests')
-        time.sleep(2)
 
         # Number of Groups:
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[12]/main/div/div[1]/div[2]/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/form/div[4]/div/div/div[2]/div[1]/div[1]/input'))).click() # Open first drop down
@@ -34,8 +33,22 @@ def log_page_1(browser):
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[21]/main/div/div[1]/div[2]/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div/div[2]/form/div[5]/div/div/div[2]/div[1]/div[1]/input'))).click()
         WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[9]/div/div/div[2]/a/div/div'))).click()
 
+        # LEDs
+        for led in led_list:
+            browser.find_element_by_css_selector("div.v-select__selections:nth-child(2) > input:nth-child(1)").send_keys(led)
+            time.sleep(1)
+            browser.find_element_by_css_selector("div.v-select__selections:nth-child(2) > input:nth-child(1)").send_keys(Keys.RETURN)
+            browser.find_element_by_xpath('/html').click() # Click off screen to reset dropdown
+
+        # Submit
+        browser.find_element_by_css_selector('div.v-stepper__content:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(1)').click()
+        print('Page 1: Experiment Complete')
+        return browser
+
+
 
 
 if __name__ == '__main__':
-    browser = connect_login(username, password)
-    log_new_experiment(browser)
+    browser = connect_login('bletson', 'baseball2046', '/Users/cameron.carverdata/geckodriver.exe')
+    time.sleep(2)
+    log_page_1(browser, led_list=['AA123', 'CJ03'])
